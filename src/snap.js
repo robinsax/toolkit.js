a1da02253cb601d5623460c1ac0b51407ea7728d
robin saxifrage <robinsaxifrage@live.ca>
2018-02-14 13:39:40 -0800

    made snap customizable

--- src/snap.js
+++ src/snap.js
@@ -1,7 +1,7 @@
 tk.snap = function(shorthand, rootElement){
+	//	Process timeouts.
 	var oArguments = [].slice.call(arguments);
 	oArguments.splice(0, 2);
-
 	var timeoutMatcher = /\.\.\.\(([0-9]+)\)/;
 	if (timeoutMatcher.test(shorthand)){
 		//	Includes sleeps.
@@ -46,24 +46,31 @@ tk.snap = function(shorthand, rootElement){
 		}
 	}
 
+	function error(){
+		throw [].slice.call(arguments).join(' ') + ' (in snap: ' + shorthand.substring(0, shorthand.length - 1) + ')';
+	}
+
 	function resolveVariable(variable){
 		var expression;
 
 		//	Resolve values.
-		if (variable.length == 0){
+		if (variable === undefined || variable.length == 0){
 			variable = '$0';
 		}
 		else if (expression = /([a-z]+)\((.*?)\)/.exec(variable)){
-			//	Function call.
-			switch(expression[1]){
-				case 'null':
-					variable = resolveVariable(expression[2]) === null;
-					break;
-				case 'notnull':
-					variable = resolveVariable(expression[2]) !== null;
-					break;
-				default:
-					throw 'No such function: ' + expression[1];
+			var name = expression[1],
+				params = tk.comprehension(expression[2].split(/\s+/), tk.fn.cleanWhitespace),
+				found = false;
+			
+			tk.iter(tk.snap.storage.functions, function(key, func){
+				if (key == name){
+					variable = func(params, resolveVariable);
+					found = true;
+					return false;
+				}
+			});
+			if (!found){
+				error('No such function:', name);
 			}
 		}
 		
@@ -84,33 +91,31 @@ tk.snap = function(shorthand, rootElement){
 			element.attr(left, right);
 		}
 		else {
-			//	Check special cases.
+			//	Locate directive.
 			var expression;
-			if (expression = /^css\(([\w\-]+)((?:\s[$\w\-]+)|)\)$/.exec(left)){
-				element.css(expression[1], resolveVariable(expression[2].trim()));
+			if (!(expression = /^([a-z]+)(?:\((.*?)\)|)$/.exec(left))){
+				error('Invalid directive:', left);
 			}
-			else if (expression = /^class\(([$\w\-]+)((?:\s[$()\w\-]+)|)((?:\s[0-9]+)|)\)$/.exec(left)){
-				var time = expression[3].length == 0 ? -1 : parseInt(expression[3].trim());
-				element.classify(
-					resolveVariable(expression[1]),
-					resolveVariable(expression[2].trim()),
-					time
-				);
-			}
-			else {
-				switch (left){
-					case 'html':
-						element.html(right);
-						break;
-					case 'text':
-						element.text(right);
-						break;
-					case 'wipe()':
-						element.html('');
-						break;
-					default:
-						throw 'Invalid left hand of expression: ' + left + ' (in ' + shorthand + ')';
+
+			var name = expression[1],
+				pure = expression[2] === undefined ? [] : expression[2].split(/\s+/),
+				params = tk.comprehension(pure, function(s){
+					s = s.trim();
+					if (s.length > 0){ return s; }
+				}),
+				found = false;
+			
+			tk.iter(tk.snap.storage.directives, function(key, directive){
+				if (name == key){
+					if (directive(params, right, element, resolveVariable) === false){
+						error('Illegal call to directive:', left);
+					}
+					found = true;
+					return false;
 				}
+			});
+			if (!found){
+				error('No such directive:', name);
 			}
 		}
 	}
@@ -130,12 +135,13 @@ tk.snap = function(shorthand, rootElement){
 	}
 
 	shorthand += '&';
-	var depthFinder = /(.*?)([>&])/g,
+	var depthFinder = /(.*?)([>&^])/g,
 		element = rootElement,
 		depth,
 		last = element;
 	while (depth = depthFinder.exec(shorthand)){
 		var siblingMode = depth[2].length > 0 && depth[2] == '&',
+			riseMode = depth[2].length > 0 && depth[2] == '^',
 			item = depth[1];
 
 		//	Parse.
@@ -143,7 +149,7 @@ tk.snap = function(shorthand, rootElement){
 			resolve = function(){ return element; };
 
 		if (match == null){
-			throw 'Invalid element reference: ' + item + ' (in ' + shorthand + ')';
+			error('Invalid element reference:', item);
 		}
 		//	Parse match for maintainability
 		var elementPolicy = match[1],
@@ -170,7 +176,7 @@ tk.snap = function(shorthand, rootElement){
 			else {
 				element = element.children(tagName + idMod + classMod);
 				if (element.empty){
-					throw 'No matched element for reference: ' + item + ' (in ' + shorthand + ')';
+					error('No matched element for reference:', item);
 				}
 				if (childIndex != null){
 					element = element.ith(childIndex);
@@ -223,11 +229,65 @@ tk.snap = function(shorthand, rootElement){
 		}
 
 		resolve();
-		if (siblingMode && element.backChain){
-			last = element;
-			element = element.back();
+		if (siblingMode){
+			if (element.backChain){
+				last = element;
+				element = element.back();
+			}
+		}
+		else if (riseMode){
+			last = element.back();
+			element = last.back();
 		}
 	}
 
 	return last;
+}
+tk.snap.storage = {
+	directives: {
+		html: function(params, right, element){
+			if (params.length > 0){ return false; }
+			element.html(right);
+		},
+		text: function(params, right, element){
+			if (params.length > 0){ return false; }
+			element.text(right);
+		},
+		remove: function(params, right, element){
+			if (params.length > 0){ return false; }
+			element.remove();
+		},
+		wipe: function(params, right, element){
+			if (params.length > 0){ return false; }
+			element.html('');
+		},
+		class: function(params, right, element, resolve){
+			if (params.length == 0){ return false; }
+			var varg = tk.varg.on(params);
+			element.classify(resolve(params[0]), resolve(varg(1, true)), +resolve(varg(2, -1)));
+		},
+		declass: function(params, right, element, resolve){
+			if (params.length == 0){ return false; }
+			var varg = tk.varg.on(params);
+			element.classify(resolve(params[0]), !resolve(varg(1, true)), +resolve(varg(2, -1)));
+		},
+		css: function(params, right, element, resolve){
+			if (params.length == 0){ return false; }
+			element.css(resolve(params[0]), resolve(params[1]));
+		}
+	},
+	functions: {
+		null: function(params, resolve){
+			return resolve(params[0]) === null;
+		},
+		notnull: function(params, resolve){
+			return resolve(params[0]) !== null;
+		}
+	}
+}
+tk.snap.directive = function(name, directive){
+	tk.snap.storage.directives[name] = directive;
+}
+tk.snap.function = function(name, func){
+	tk.snap.storage.functions[name] = func;
 }
--- toolkit.js
+++ toolkit.js
@@ -357,9 +357,9 @@ function createToolkit(){
 
 	/* ---- Shorthand notation ---- */
 	tk.snap = function(shorthand, rootElement){
+		//	Process timeouts.
 		var oArguments = [].slice.call(arguments);
 		oArguments.splice(0, 2);
-	
 		var timeoutMatcher = /\.\.\.\(([0-9]+)\)/;
 		if (timeoutMatcher.test(shorthand)){
 			//	Includes sleeps.
@@ -404,24 +404,31 @@ function createToolkit(){
 			}
 		}
 	
+		function error(){
+			throw [].slice.call(arguments).join(' ') + ' (in snap: ' + shorthand.substring(0, shorthand.length - 1) + ')';
+		}
+	
 		function resolveVariable(variable){
 			var expression;
 	
 			//	Resolve values.
-			if (variable.length == 0){
+			if (variable === undefined || variable.length == 0){
 				variable = '$0';
 			}
 			else if (expression = /([a-z]+)\((.*?)\)/.exec(variable)){
-				//	Function call.
-				switch(expression[1]){
-					case 'null':
-						variable = resolveVariable(expression[2]) === null;
-						break;
-					case 'notnull':
-						variable = resolveVariable(expression[2]) !== null;
-						break;
-					default:
-						throw 'No such function: ' + expression[1];
+				var name = expression[1],
+					params = tk.comprehension(expression[2].split(/\s+/), tk.fn.cleanWhitespace),
+					found = false;
+				
+				tk.iter(tk.snap.storage.functions, function(key, func){
+					if (key == name){
+						variable = func(params, resolveVariable);
+						found = true;
+						return false;
+					}
+				});
+				if (!found){
+					error('No such function:', name);
 				}
 			}
 			
@@ -442,33 +449,31 @@ function createToolkit(){
 				element.attr(left, right);
 			}
 			else {
-				//	Check special cases.
+				//	Locate directive.
 				var expression;
-				if (expression = /^css\(([\w\-]+)((?:\s[$\w\-]+)|)\)$/.exec(left)){
-					element.css(expression[1], resolveVariable(expression[2].trim()));
+				if (!(expression = /^([a-z]+)(?:\((.*?)\)|)$/.exec(left))){
+					error('Invalid directive:', left);
 				}
-				else if (expression = /^class\(([$\w\-]+)((?:\s[$()\w\-]+)|)((?:\s[0-9]+)|)\)$/.exec(left)){
-					var time = expression[3].length == 0 ? -1 : parseInt(expression[3].trim());
-					element.classify(
-						resolveVariable(expression[1]),
-						resolveVariable(expression[2].trim()),
-						time
-					);
-				}
-				else {
-					switch (left){
-						case 'html':
-							element.html(right);
-							break;
-						case 'text':
-							element.text(right);
-							break;
-						case 'wipe()':
-							element.html('');
-							break;
-						default:
-							throw 'Invalid left hand of expression: ' + left + ' (in ' + shorthand + ')';
+	
+				var name = expression[1],
+					pure = expression[2] === undefined ? [] : expression[2].split(/\s+/),
+					params = tk.comprehension(pure, function(s){
+						s = s.trim();
+						if (s.length > 0){ return s; }
+					}),
+					found = false;
+				
+				tk.iter(tk.snap.storage.directives, function(key, directive){
+					if (name == key){
+						if (directive(params, right, element, resolveVariable) === false){
+							error('Illegal call to directive:', left);
+						}
+						found = true;
+						return false;
 					}
+				});
+				if (!found){
+					error('No such directive:', name);
 				}
 			}
 		}
@@ -488,12 +493,13 @@ function createToolkit(){
 		}
 	
 		shorthand += '&';
-		var depthFinder = /(.*?)([>&])/g,
+		var depthFinder = /(.*?)([>&^])/g,
 			element = rootElement,
 			depth,
 			last = element;
 		while (depth = depthFinder.exec(shorthand)){
 			var siblingMode = depth[2].length > 0 && depth[2] == '&',
+				riseMode = depth[2].length > 0 && depth[2] == '^',
 				item = depth[1];
 	
 			//	Parse.
@@ -501,7 +507,7 @@ function createToolkit(){
 				resolve = function(){ return element; };
 	
 			if (match == null){
-				throw 'Invalid element reference: ' + item + ' (in ' + shorthand + ')';
+				error('Invalid element reference:', item);
 			}
 			//	Parse match for maintainability
 			var elementPolicy = match[1],
@@ -528,7 +534,7 @@ function createToolkit(){
 				else {
 					element = element.children(tagName + idMod + classMod);
 					if (element.empty){
-						throw 'No matched element for reference: ' + item + ' (in ' + shorthand + ')';
+						error('No matched element for reference:', item);
 					}
 					if (childIndex != null){
 						element = element.ith(childIndex);
@@ -581,13 +587,67 @@ function createToolkit(){
 			}
 	
 			resolve();
-			if (siblingMode && element.backChain){
-				last = element;
-				element = element.back();
+			if (siblingMode){
+				if (element.backChain){
+					last = element;
+					element = element.back();
+				}
+			}
+			else if (riseMode){
+				last = element.back();
+				element = last.back();
 			}
 		}
 	
 		return last;
+	}
+	tk.snap.storage = {
+		directives: {
+			html: function(params, right, element){
+				if (params.length > 0){ return false; }
+				element.html(right);
+			},
+			text: function(params, right, element){
+				if (params.length > 0){ return false; }
+				element.text(right);
+			},
+			remove: function(params, right, element){
+				if (params.length > 0){ return false; }
+				element.remove();
+			},
+			wipe: function(params, right, element){
+				if (params.length > 0){ return false; }
+				element.html('');
+			},
+			class: function(params, right, element, resolve){
+				if (params.length == 0){ return false; }
+				var varg = tk.varg.on(params);
+				element.classify(resolve(params[0]), resolve(varg(1, true)), +resolve(varg(2, -1)));
+			},
+			declass: function(params, right, element, resolve){
+				if (params.length == 0){ return false; }
+				var varg = tk.varg.on(params);
+				element.classify(resolve(params[0]), !resolve(varg(1, true)), +resolve(varg(2, -1)));
+			},
+			css: function(params, right, element, resolve){
+				if (params.length == 0){ return false; }
+				element.css(resolve(params[0]), resolve(params[1]));
+			}
+		},
+		functions: {
+			null: function(params, resolve){
+				return resolve(params[0]) === null;
+			},
+			notnull: function(params, resolve){
+				return resolve(params[0]) !== null;
+			}
+		}
+	}
+	tk.snap.directive = function(name, directive){
+		tk.snap.storage.directives[name] = directive;
+	}
+	tk.snap.function = function(name, func){
+		tk.snap.storage.functions[name] = func;
 	}
 
 	/* ---- Selection ---- */
